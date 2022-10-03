25f544bf9dcc9731b314281e58693d3cf463736c
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2022-10-03 23:41:13 +0200

    update SearchFrom

---
+++ vue-cli/capital-search/src/components/SearchForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="text" v-model="inputValue">
+    <button type="submit" @click="findCapital">find</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, defineProps} from "vue";
+// https://github.com/johnsoncodehk/volar/issues/460
+interface Props {
+  name: string,
+  findCapital: () => void
+}
+
+defineProps<Props>();
+
+export default defineComponent({
+  name: "SearchForm",
+  props: {
+    modelValue: {
+      required: true,
+      type: String
+    },
+    findCapital: {
+      required: true,
+      type: Function
+    }
+  },
+  computed: {
+    inputValue: {
+      get():string | undefined {
+        return this.modelValue;
+      },
+      set(value: string):void {
+        this.$emit('update:modelValue', value)
+      }
+    }
+  }
+})
+</script>
--- vue-cli/capital-search/src/views/HomeView.vue
+++ vue-cli/capital-search/src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home">
-    <h1 class="capital-app-title">Capital App</h1>
-    <form>
-      <input type="text" v-model="inputedCountry">
-      <button type="submit" @click="find($event)">find</button>
+    <h1 class="home__title">Capital App</h1>
+    <form class="home__form"> 
+      <SearchForm v-model="inputedCountry" :findCapital="find"/>
+
       <p v-if="receivedCapital">{{receivedCapital}}</p>
+      <SpinnerLoader v-else-if="isLoading"/>
+      <p v-if="errorMessage">{{errorMessage}}</p>
 
     </form>
   </div>
@@ -12,30 +14,45 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import CapitalAPI from "../api/capital"
+import CapitalAPI from "../api/capital";
+import SpinnerLoader from "../components/SpinnerLoader.vue";
+import SearchForm from "../components/SearchForm.vue";
 
 export default defineComponent({
   name: 'HomeView',
   components: {
+    SpinnerLoader,
+    SearchForm
   },
   data() {
     return {
       inputedCountry: "",
-      receivedCapital: ""
+      receivedCapital: "",
+      errorMessage: "",
+      isLoading: false
     }
   },
   methods: {
     async find(event: Event) {
+      this.isLoading = true;
+      this.receivedCapital = "";
       event.preventDefault();
-      const res = await CapitalAPI.findCapital(this.inputedCountry);
-      this.receivedCapital = res;
+      try {
+        const res = await CapitalAPI.findCapital(this.inputedCountry);
+        res ? this.receivedCapital = res : this.errorMessage = `Can not find capital for ${this.inputedCountry} country`
+      } catch (err) {
+        this.errorMessage = "Server problem"
+      } finally {
+        this.isLoading = false;
+        setTimeout(() => this.errorMessage="",2000)
+      }
     }
   }
 });
 </script>
 
 <style lang="sass">
-  .capital-app-title
+  .home__title
     text-align: center
     margin-top: 50px
 </style>
