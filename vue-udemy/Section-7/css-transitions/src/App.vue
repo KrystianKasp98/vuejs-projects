52c30eb5f81d6b7907f55078d8f6edd3b6ecb20b
KrystianKasp98 <krystiankasprzak98byd@gmail.com>
2022-10-25 08:35:58 +0200

    update section-7

---
+++ vue-udemy/Section-7/css-transitions/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button type="button" @click="handleFlag()">Toggle</button>
+    <!-- 3 classes v-enter-from, v-enter-active, v-enter-to -->
+    <transition>
+      <h2 v-if="flag">Hello world</h2>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      flag: false
+    }
+  },
+  methods: {
+    handleFlag() {
+      this.flag = !this.flag;
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: grey;
+}
+</style>
